aa69fae99531e773aa3f6b406e4a79ad674dfaba
Abdullah Khalid bin Ammar <62870638+A222moq3e@users.noreply.github.com>
2023-12-27 18:40:43 +0300

    more tab centered

--- main.js
+++ main.js
@@ -2,40 +2,48 @@ let dataBox = document.querySelector('.data');
 let title = document.querySelector('.data h2');
 let position = document.querySelector('.data .position');
 let tab;
-let opsite_tab;
+let opsiteTab;
+let windowPosition = [window.screenX+500,window.screenY+290]
 if(localStorage.getItem('tab2')){
     title.innerHTML = 'You alredy open 2 tabs!, click restart Button'
+    localStorage.removeItem('tab1');localStorage.removeItem('tab2');window.location.reload();
 }else if(localStorage.getItem('tab1')){
     tab='tab2';
-    opsite_tab ='tab1';
-    localStorage.setItem(tab,[window.screenX,window.screenY])
+    opsiteTab ='tab1';
+    localStorage.setItem(tab,getPostion())
     title.innerHTML = 'second tab'
 }else{
     tab='tab1';
-    opsite_tab ='tab2';
-    localStorage.setItem(tab,[window.screenX,window.screenY])
+    opsiteTab ='tab2';
+    localStorage.setItem(tab,getPostion())
     title.innerHTML = 'first tab'
 }
 
 setInterval(function(){ 
-    localStorage.setItem(tab,[window.screenX,window.screenY])
+    localStorage.setItem(tab,getPostion())
 }, 100);
 
+function getPostion(){
+    // return [window.innerHeight/2,window.innerWidth/2]
+    return [window.screenX+500,window.screenY+290]
+}
 
 // Canvas
-var canvas=document.getElementById("canvas");
-var ctx=canvas.getContext("2d");
+var canvas= document.getElementById("canvas");
+var ctx= canvas.getContext("2d");
 
 
 window.addEventListener('storage',()=>{
-    let storageOpsitePosition= (localStorage.getItem(opsite_tab)).split(',')
+    let storageOpsitePosition= (localStorage.getItem(opsiteTab)).split(',')
     console.log(storageOpsitePosition);
     position.innerHTML = storageOpsitePosition
     
         // Start a new Path
     ctx.beginPath();
     ctx.strokeStyle = '#ff0000';
-    ctx.moveTo(150, 150);
+    if(tab=='tab1') ctx.strokeStyle = '#ff5500';
+    if(tab=='tab2') ctx.strokeStyle = '#0055ff';
+    ctx.moveTo(750, 390);
     // ctx.moveTo(canvas.offsetHeight, canvas.offsetWidth);
     ctx.lineTo(storageOpsitePosition[0], storageOpsitePosition[1]);
 
